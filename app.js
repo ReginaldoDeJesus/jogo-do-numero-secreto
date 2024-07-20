a3ac8f255b314cb7806c41983ae040a01b10be56
Reginaldo de Jesus <reginaldosilva007@hotmail.com>
2024-07-20 14:40:20 -0300

    Aterado limite para 50

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 50;
 let numeroSecreto = gerarUmNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto){
 
 function exibirMensagemInicial(){
     exibirTextoNaTela("h1","Jogo do número secreto");
-    exibirTextoNaTela("p","Escolha um número de 1 e 4:");
+    exibirTextoNaTela("p",`Escolha um número de 1 e ${numeroLimite}:`);
 }
 
 exibirMensagemInicial();
